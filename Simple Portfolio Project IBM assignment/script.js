f78bf017c1020316eb895627ab90aec3c191fd42
Tasbeeh TechBoy <tasbeehullah.info@gmail.com>
2023-12-06 02:19:25 +0500

    Add More features in Css

---
+++ Simple Portfolio Project IBM assignment/script.js
@@ -0,0 +1,32 @@
+
+
+// submit button popup
+
+
+
+
+
+
+
+
+
+// Pseudo code for the Submit Button
+
+// Step 1: Get the submit button element
+// submitBtn = document.getElementById('submit');
+
+// Step 2: Add a click event listener to the submit button
+// submitBtn.addEventListener('click', function (event) {
+
+// Step 3: Prevent the default form submission behavior
+// event.preventDefault();
+
+// Step 4: Check if the form is valid (optional)
+// You can add validation logic here if needed
+
+// Step 5: Display a thank you message in a popup
+// alert('Thank you for your recommendation!');
+
+// Optional: You can reset the form if needed
+// document.getElementById('form_recommendation').reset();
+// });
