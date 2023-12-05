b13c33c7524fa2ec5c42a4b1b8d71f816f5ccc67
Tasbeeh TechBoy <tasbeehullah.info@gmail.com>
2023-12-06 03:50:02 +0500

    Adding Javascript file

---
+++ Pseudocode.js
@@ -0,0 +1,74 @@
+
+// Define a variable to store the navbar element
+// var navbar = document.getElementById("navbar");
+
+// Define a function to toggle the navbar visibility when the user clicks on the menu icon
+// function toggleNavbar() {
+// If the navbar is hidden, show it
+//     if (navbar.style.display == "none") {
+//         navbar.style.display = "block";
+//     }
+// Otherwise, hide it
+//     else {
+//         navbar.style.display = "none";
+//     }
+// }
+
+// Add an event listener to the menu icon to call the toggleNavbar function when clicked
+// var menuIcon = document.getElementById("menu-icon");
+// menuIcon.addEventListener("click", toggleNavbar);
+
+// Define a variable to store the home button element
+// var homeButton = document.getElementById("home-button");
+
+
+
+
+
+
+// Define a function to scroll to the top of the page when the user clicks on the home button
+// function scrollToTop() {
+// Use the window.scrollTo method to scroll to the top of the page with a smooth behavior
+//     window.scrollTo({
+//         top: 0,
+//         behavior: "smooth"
+//     });
+// }
+
+// Add an event listener to the home button to call the scrollToTop function when clicked
+// homeButton.addEventListener("click", scrollToTop);
+
+// Define variables to store the section elements
+// var aboutSection = document.getElementById("about-section");
+// var skillsSection = document.getElementById("skills-section");
+// var projectsSection = document.getElementById("projects-section");
+// var recommendationSection = document.getElementById("recommendation-section");
+
+// Define a function to scroll to a specific section when the user clicks on the corresponding navbar link
+// function scrollToSection(section) {
+// Use the element.scrollIntoView method to scroll to the section with a smooth behavior
+//     section.scrollIntoView({
+//         behavior: "smooth"
+//     });
+// }
+
+// Add event listeners to the navbar links to call the scrollToSection function with the appropriate section as an argument
+// var aboutLink = document.getElementById("about-link");
+// aboutLink.addEventListener("click", function () {
+//     scrollToSection(aboutSection);
+// });
+
+// var skillsLink = document.getElementById("skills-link");
+// skillsLink.addEventListener("click", function () {
+//     scrollToSection(skillsSection);
+// });
+
+// var projectsLink = document.getElementById("projects-link");
+// projectsLink.addEventListener("click", function () {
+//     scrollToSection(projectsSection);
+// });
+
+// var recommendationLink = document.getElementById("recommendation-link");
+// recommendationLink.addEventListener("click", function () {
+//     scrollToSection(recommendationSection);
+// });
