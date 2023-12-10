217eab05e7783fdf588ba94540d6ca6d4b5e7913
Tasbeeh TechBoy <tasbeehullah.info@gmail.com>
2023-12-11 02:14:24 +0500

    Adding Javascript

--- Pseudocode.js
+++ Pseudocode.js
@@ -1,74 +1,41 @@
+// let submitButton = document.getElementById('submitBtn')
 
-// Define a variable to store the navbar element
-// var navbar = document.getElementById("navbar");
 
-// Define a function to toggle the navbar visibility when the user clicks on the menu icon
-// function toggleNavbar() {
-// If the navbar is hidden, show it
-//     if (navbar.style.display == "none") {
-//         navbar.style.display = "block";
+
+// submitButton.addEventListener('click', function (event) {
+
+//     event.preventDefault();
+
+//     const isFormValid = validateForm();
+//     if (isFormValid) {
+
+//         alert('Thank You For Submition');
+
+//         document.getElementById('form_recommendation').reset()
+
+
 //     }
-// Otherwise, hide it
-//     else {
-//         navbar.style.display = "none";
-//     }
-// }
-
-// Add an event listener to the menu icon to call the toggleNavbar function when clicked
-// var menuIcon = document.getElementById("menu-icon");
-// menuIcon.addEventListener("click", toggleNavbar);
-
-// Define a variable to store the home button element
-// var homeButton = document.getElementById("home-button");
+// })
 
 
 
+// Pseudo code for the Submit Button
 
+// Step 1: Get the submit button element
+// submitBtn = document.getElementById('submit');
 
+// Step 2: Add a click event listener to the submit button
+// submitBtn.addEventListener('click', function (event) {
 
-// Define a function to scroll to the top of the page when the user clicks on the home button
-// function scrollToTop() {
-// Use the window.scrollTo method to scroll to the top of the page with a smooth behavior
-//     window.scrollTo({
-//         top: 0,
-//         behavior: "smooth"
-//     });
-// }
+// Step 3: Prevent the default form submission behavior
+// event.preventDefault();
 
-// Add an event listener to the home button to call the scrollToTop function when clicked
-// homeButton.addEventListener("click", scrollToTop);
+// Step 4: Check if the form is valid (optional)
+// You can add validation logic here if needed
 
-// Define variables to store the section elements
-// var aboutSection = document.getElementById("about-section");
-// var skillsSection = document.getElementById("skills-section");
-// var projectsSection = document.getElementById("projects-section");
-// var recommendationSection = document.getElementById("recommendation-section");
+// Step 5: Display a thank you message in a popup
+// alert('Thank you for your recommendation!');
 
-// Define a function to scroll to a specific section when the user clicks on the corresponding navbar link
-// function scrollToSection(section) {
-// Use the element.scrollIntoView method to scroll to the section with a smooth behavior
-//     section.scrollIntoView({
-//         behavior: "smooth"
-//     });
-// }
-
-// Add event listeners to the navbar links to call the scrollToSection function with the appropriate section as an argument
-// var aboutLink = document.getElementById("about-link");
-// aboutLink.addEventListener("click", function () {
-//     scrollToSection(aboutSection);
-// });
-
-// var skillsLink = document.getElementById("skills-link");
-// skillsLink.addEventListener("click", function () {
-//     scrollToSection(skillsSection);
-// });
-
-// var projectsLink = document.getElementById("projects-link");
-// projectsLink.addEventListener("click", function () {
-//     scrollToSection(projectsSection);
-// });
-
-// var recommendationLink = document.getElementById("recommendation-link");
-// recommendationLink.addEventListener("click", function () {
-//     scrollToSection(recommendationSection);
+// Optional: You can reset the form if needed
+// document.getElementById('form_recommendation').reset();
 // });
